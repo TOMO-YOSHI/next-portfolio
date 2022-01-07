5cb2965ac26630d578f74bbedab2abea2c7032e9
tomoy <tomohiro@tellext.com>
2022-01-06 23:58:38 -0800

    modify routing

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import Layout from '../components/Layout';
 
 export default function ProfilePage() {
@@ -5,6 +6,12 @@ export default function ProfilePage() {
         <Layout title="TOMO's Profile">
             <h1>About TOMO</h1>
             <p>Hello, my name is Tomohiro Yoshida. I'm a Full-Stack Web Developer with a background in public service. I am proficient in practical web development skills including both server-side and client-side to create a user-friendly application. Through projects and practical experience, I am accustomed to collaborating with team members from different professions.</p>
+            <Link href={'/'}>
+                <a>Welcome</a>
+            </Link>
+            <Link href={'/works'}>
+                <a>Works</a>
+            </Link>
         </Layout>
     )
 }
