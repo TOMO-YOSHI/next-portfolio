b856f7ef8ffb84852f555a3be76b26fcc8d51762
TOMO-YOSHI <lucky.paddy10@gmail.com>
2022-01-27 01:09:51 -0800

    add go to top at the bottom in mobile layout

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,21 @@
+import * as Scroll from 'react-scroll';
 import styles from '../styles/Footer.module.scss';
 
 const Footer = () => {
+
+    const scrollToTop = () => {
+        Scroll.animateScroll.scrollToTop({
+            duration: 500,
+            delay: 100,
+            smooth: true,
+        })
+    }
+
     return (
         <footer className={styles.footer}>
+            <p
+                onClick={scrollToTop}
+                className={styles.goToTopP}>Go to Top</p>
             <p>&copy; 2022 Tomohiro Yoshida. All Rights Reserved</p>
         </footer>
     )
